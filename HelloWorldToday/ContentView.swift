6b59c02e4cdbe3b1a852037a956dfbbf129d0166
Michael Craun <michael.craun@gmail.com>
2020-12-06 15:32:08 -0500

    Migrating project to Github

--- HelloWorldToday/ContentView.swift
+++ HelloWorldToday/ContentView.swift
@@ -10,7 +10,43 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        HStack {
+            Image("paulos-nice-photo")
+                .resizable()
+                .scaledToFill()
+                .clipShape(Circle())
+                .overlay(Circle()
+                    .stroke(lineWidth: 3)
+                    .foregroundColor(.white))
+                .padding()
+                .frame(width: 150)
+                .shadow(radius: 5)
+            
+            VStack {
+                Text("Paulo Dichone")
+                    .font(.title)
+                    .foregroundColor(.white)
+                
+                Text("buildappswithpaulo.com")
+                    .font(.subheadline)
+                    .foregroundColor(.white)
+                
+                HStack {
+                    Image(systemName: "t.square.fill")
+                        .foregroundColor(.pink)
+                    
+                    Text(": @buildappswithme")
+                        .font(.subheadline)
+                        .foregroundColor(.white)
+                        .bold()
+                }
+            }
+        }
+        .frame(width: 340, height: 180)
+        .padding()
+        .background(Color.orange)
+        .cornerRadius(8)
+        .shadow(radius: 5)
     }
 }
 
